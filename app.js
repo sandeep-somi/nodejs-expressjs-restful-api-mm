b85f7d1bd8df647ffa87fff774c9da59a48979bd
Sandeep Singh <soami.ssr@gmail.com>
2019-01-14 14:13:56 +0530

    fixed access control allow origin etc settings

--- app.js
+++ app.js
@@ -5,12 +5,34 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config');
 
+mongoose.connect(config.connectURI, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+
 const productRoutes = require('./api/route/products');
 const orderRoutes = require('./api/route/orders');
 const userRoutes = require('./api/route/users');
 
-mongoose.connect(config.connectURI, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
+// app.use((req, res, next) => {
+//   res.header('Access-Contorl-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Contol-Allow-Methods', 'PUT, POST, PATCH, PUT, DELETE');
+//     return res.status(200).json({});
+//   }
+//   next();
+// })
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Origin-Headers', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, PATCH, DELETE');
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", 'PUT, PATCH, POST, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,16 +42,6 @@ app.use('/uploads', express.static('uploads'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 app.use('/users', userRoutes);
-
-app.use((req, res, next) => {
-  res.header('Access-Contorl-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if(req.method === 'OPTIONS') {
-    res.header('Access-Contol-Allow-Methods', 'PUT, POST, PATCH, PUT, DELETE');
-    return res.status(200).json();
-  }
-})
-
 
 app.use((req, res, next) => {
   const error = new Error("Not Found!");
